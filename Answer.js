0417713e54b5042128b44df4d0e14fd6759ef242
Jin Dai <jindai1783@gmail.com>
2015-03-17 14:13:33 +0000

    Resolved IE8 compatibility issue

--- Answer.js
+++ Answer.js
@@ -1,3 +1,7 @@
+if (belowIE9) {
+  addPolyfills();
+}
+
 var $ = function (selector) {
 
   selectorSymbols = /(?=#)|(?=\.)/;
@@ -47,4 +51,85 @@ function selector2(n) {
 
 function selector3(n) {
   return getElements(selectors[2]).indexOf(n) != -1;
+}
+
+function belowIE9() {
+  return !document.addEventListener;
+}
+
+function addPolyfills() {
+  
+  Array.prototype.filter = function(fun/*, thisArg*/) {
+    'use strict';
+    if (this === void 0 || this === null) {
+      throw new TypeError();
+    }
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (typeof fun !== 'function') {
+      throw new TypeError();
+    }
+    var res = [];
+    var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+    for (var i = 0; i < len; i++) {
+      if (i in t) {
+        var val = t[i];
+        if (fun.call(thisArg, val, i, t)) {
+          res.push(val);
+        }
+      }
+    }
+    return res;
+  };
+
+  Array.prototype.indexOf = function(searchElement, fromIndex) {
+    var k;
+    if (this == null) {
+      throw new TypeError('"this" is null or not defined');
+    }
+    var O = Object(this);
+    var len = O.length >>> 0;
+    if (len === 0) {
+      return -1;
+    }
+    var n = +fromIndex || 0;
+    if (Math.abs(n) === Infinity) {
+      n = 0;
+    }
+    if (n >= len) {
+      return -1;
+    }
+    k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+    while (k < len) {
+      if (k in O && O[k] === searchElement) {
+        return k;
+      }
+      k++;
+    }
+    return -1;
+  };
+
+  document.getElementsByClassName = function(search) {
+    var d = document, elements, pattern, i, results = [];
+    if (d.querySelectorAll) {
+      return d.querySelectorAll("." + search);
+    }
+    if (d.evaluate) {
+      pattern = ".//*[contains(concat(' ', @class, ' '), ' " + search + " ')]";
+      elements = d.evaluate(pattern, d, null, 0, null);
+      while ((i = elements.iterateNext())) {
+        results.push(i);
+      }
+    } else {
+      elements = d.getElementsByTagName("*");
+      pattern = new RegExp("(^|\\s)" + search + "(\\s|$)");
+      for (i = 0; i < elements.length; i++) {
+        if ( pattern.test(elements[i].className) ) {
+          results.push(elements[i]);
+        }
+      }
+    }
+    return results;
+  };
+
 }
--- README.md
+++ README.md
@@ -9,3 +9,7 @@ This repo is a note of building a JS selection engine that works like JQuery. It
 **KISS principle:** most systems work best if they are kept simple rather than made complicated. I have only chosen the  most easy-to-read syntax.
 
 **YAGNI principle:** a programmer should not add functionality until deemed necessary. The code is designed to pass the test and no more.
+
+---17 March update---
+
+**Backward compatible:** if IE8 is detected, polyfills will be added so  the script is still functional.
